c8d667cca72094c5556d1e4e74a38f7d6880ac1a
Rajeshwarikota <kotarajeswari003.com>
2023-03-22 20:30:04 +0530

    [Rajeshwari] Adding NLog And CRUD Oertaions

---
+++ NlogProject/Program.cs
@@ -0,0 +1,21 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlogProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("....NLog...");
+            Adding.Sum(1,3);
+            SwappingNumber.Swap();
+            Console.ReadLine();
+        }
+    }
+}
+
---
+++ NlogProject/SwappingNumber.cs
@@ -0,0 +1,29 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NlogProject
+{
+    class SwappingNumber
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public static void Swap()
+        {
+            int a = 5;
+            int b = 10;
+
+            logger.Info("Before swapping: a = {0}, b = {1}", a, b);
+
+            // Swap the values of a and b
+            int temp = a;
+            a = b;
+            b = temp;
+
+            logger.Info("After swapping: a = {0}, b = {1}", a, b);
+        }
+    }
+}
